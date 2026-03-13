65f85af9455fa6f70d69b39521380dd70e7fd484
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-13 10:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here!
+let list = document.getElementById("infi-list");
+let count = 1;
 
+function addItem(nums) {
+	for(let i=0; i<nums; i++){
+          let li = document.createElement("li");
+		li.innerText = "Item "+count;
+		list.appendChild(li);
+		count++;
+	}
+}
+
+addItem(10);
+
+list.addEventListener("scroll", function(){
+
+let scrollTop = list.scrollTop;
+let visibleHeight = list.clientHeight;
+let fullHeight = list.scrollHeight;
+
+if(scrollTop + visibleHeight >= fullHeight - 5){
+
+addItem(2);
+
+}
+
+});
